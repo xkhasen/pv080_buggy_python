51521eaf11c0eff62725444ca27328fa65c9322c
xkhasen <khassenov.adil2001@gmail.com>
2022-04-28 16:44:44 +0200

    some fixesd

--- code.py
+++ code.py
@@ -5,15 +5,15 @@ import subprocess
 import flask
 
 
-def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=file)
+def transcode_file(filename):
+    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)
 
 
 def load_config(filename):
     # Load a configuration file into YAML
-    stream = file.open(filename, "w")
-    config = yaml.load(stream)
+    stream = filename.open(filename, "w")
+    yaml.load(stream)
 
 
 def authenticate(password):
@@ -26,13 +26,11 @@ def fetch_website(urllib_version, url):
     # Import the requested version of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
-    http = urllib.PoolManager()
+    http = urllib_version.PoolManager()
     r = http.request('GET', url)
     return r.data
 
 
-
-@app.route("/")
 def index():
     version = flask.request.args.get("urllib_version")
     url = flask.request.args.get("url")
